5110c306ee05a75cb09ef16527dcc63b3bab4850
ismaelJos <ismaelejano@gmail.com>
2023-10-06 18:40:38 +0100

    nuevas tareas

--- unidad-1/Tareas/bucles_y_condiciones3/src/main/java/Ejercicio6.java
+++ unidad-1/Tareas/bucles_y_condiciones3/src/main/java/Ejercicio6.java
@@ -9,13 +9,7 @@ public class Ejercicio6 {
      * @param args
      */
     public static void main(String[] args) {
-        for (int numero = 1; numero <= 1000; numero++) {
-            int decimas = 0;
-            for (int i = numero; i >= 1; i=i/10) {
-                decimas++;
-            }
 
-        }
 
     }
 }
