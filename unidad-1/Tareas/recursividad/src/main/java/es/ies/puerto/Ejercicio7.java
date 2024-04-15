e7e83513468a4683830a4a953ac4cef941956399
IsmaelJos <isamelejano@gmail.com>
2024-04-15 12:59:56 +0100

    tarea

--- unidad-1/Tareas/recursividad/src/main/java/es/ies/puerto/Ejercicio7.java
+++ unidad-1/Tareas/recursividad/src/main/java/es/ies/puerto/Ejercicio7.java
@@ -1,4 +1,21 @@
 package es.ies.puerto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejercicio7 {
+    List<Integer> lista = new ArrayList<>();
+    public List<Integer> cuentaRegresiva(int num){
+        for (int i = num; i > 0; i--) {
+            lista.add(i);
+        }
+        return lista;
+    }
+    public List<Integer> cuentaRegresivaRecursiva(int num){
+        lista.add(num);
+        if (num == 1){
+            return lista;
+        }
+        return cuentaRegresiva(num-1);
+    }
 }
